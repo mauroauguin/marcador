169568032c77bed3fade757319dcf1019046121f
mauroauguin <mauroauguin@hotmail.com>
2024-10-26 11:52:46 -0300

    Marcador blackpadel, sin boton control

--- core/views.py
+++ core/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Marcador
+import uuid
 
 def create_marcador(request):
     new_marcador = Marcador.objects.create()
     return redirect('view_marcador', marcador_id=new_marcador.id)
 
-def view_marcador(request, marcador_id):
+def view_marcador(request):
+    # Intenta obtener el marcador por ID
     try:
-        marcador = Marcador.objects.get(id=marcador_id)
+        marcador = Marcador.objects.get(id=1)  # Cambiado a un entero
     except Marcador.DoesNotExist:
-        raise Http404("Marcador no encontrado")
+        # Si no existe, crea un nuevo marcador sin especificar el ID
+        marcador = Marcador(campeonato="Campeonato Default", jugador1="Jugador 1", jugador2="Jugador 2")
+        marcador.save()  # Guarda el nuevo marcador en la base de datos
     return render(request, 'home.html', {'marcador': marcador})
 
-def admin_marcador(request, marcador_id):
+def admin_marcador(request):
     try:
-        marcador = Marcador.objects.get(id=marcador_id)
+        marcador = Marcador.objects.get(id=1)  # Cambiado a un entero
     except Marcador.DoesNotExist:
         raise Http404("Marcador no encontrado")
-    return render(request, 'admin.html', {'marcador': marcador, 'marcador_id': marcador_id})
+    return render(request, 'admin.html', {'marcador': marcador})  # Renderiza admin.html
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
